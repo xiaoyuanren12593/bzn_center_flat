19815650dfda821a1fed75c046d5867be1ec23ab
liuxiang <liuxiang@baozhunniu.com>
2019-09-12 20:43:03 +0800

    理赔表逻辑更改 2019-9-12

--- bzn_center_flat/job_dw_level/src/main/scala/bzn/dw/premium/DwPolicyClaimDetail.scala
+++ bzn_center_flat/job_dw_level/src/main/scala/bzn/dw/premium/DwPolicyClaimDetail.scala
@@ -48,17 +48,26 @@ object DwPolicyClaimDetail extends SparkUtil with Until{
     /**
       * 读取保单表
       */
-    val odsPolicyDetail = sqlContext.sql("select policy_id,policy_code,product_code,policy_status from odsdb.ods_policy_detail")
+    val odsPolicyDetail = sqlContext.sql("select policy_id,policy_code,product_code," +
+      "holder_name,policy_status from odsdb.ods_policy_detail")
 
     /**
       * 读取理赔表
       */
-    val odsClaimDetailOne = sqlContext.sql("select id, clean(case_no) as case_no, clean(policy_no) as policy_no, cast(clean(risk_date) as timestamp) as risk_date, " +
-      "cast(clean(report_date) as timestamp) as report_date, clean(risk_name) as risk_name, clean(risk_cert_no) as risk_cert_no, clean(mobile) as mobile, clean(insured_company) as insured_company, " +
-      "cast(clean(pre_com) as decimal(14,4)) as pre_com, clean(disable_level) as disable_level, clean(scene) as scene, clean(case_type) as case_type, clean(case_status) as case_status, " +
-      "cast(clean(case_close_date) as timestamp) as case_close_date, cast(clean(hos_benefits) as decimal(14,4)) as hos_benefits, cast(clean(medical_coverage) as decimal(14,4)) as medical_coverage, " +
-      "cast(clean(delay_payment) as decimal(14,4)) as delay_payment, cast(clean(disable_death_payment) as decimal(14,4)) as disable_death_payment, cast(clean(final_payment) as decimal(14,4)) as final_payment " +
-      "from odsdb.ods_claims_detail")
+    val odsClaimDetailOne = sqlContext.sql("select id, clean(case_no) as case_no, " +
+      "clean(policy_no) as policy_no, " +
+      "cast(clean(risk_date) as timestamp) as risk_date, " +
+      "cast(clean(report_date) as timestamp) as report_date, clean(risk_name) as risk_name, " +
+      "clean(risk_cert_no) as risk_cert_no, clean(mobile) as mobile, " +
+      "clean(insured_company) as insured_company, " +
+      "pre_com, clean(disable_level) as disable_level, clean(scene) as scene, " +
+      "clean(case_type) as case_type, clean(case_status) as case_status, " +
+      "cast(clean(case_close_date) as timestamp) as case_close_date, " +
+      "cast(clean(hos_benefits) as decimal(14,4)) as hos_benefits, " +
+      "cast(clean(medical_coverage) as decimal(14,4)) as medical_coverage, " +
+      "cast(clean(delay_payment) as decimal(14,4)) as delay_payment, " +
+      "cast(clean(disable_death_payment) as decimal(14,4)) as disable_death_payment," +
+      "final_payment from odsdb.ods_claims_detail")
 
     /**
       * 如果最终赔付有值就用最终赔付，如果没有值就用预估赔付
@@ -68,33 +77,68 @@ object DwPolicyClaimDetail extends SparkUtil with Until{
         val id: String = clean(x.getAs[Long]("id").toString)
 
         val preCom: String = clean(x.getAs[String]("pre_com"))
-        val preComRes: BigDecimal = if (preCom == null) new BigDecimal(0) else new BigDecimal(preCom)
-
         val finalPayment: String = clean(x.getAs[String]("final_payment"))
-        val finalPaymentRes = if (finalPayment == null) new BigDecimal(0) else new BigDecimal(finalPayment)
-
-        val resPay = if (finalPaymentRes.compareTo(new BigDecimal(0)) > 0) finalPaymentRes else preComRes
+        val finalPaymentRes = if (finalPayment == null || finalPayment == "" ) preCom else finalPayment
         //保单号  预估赔付   最终赔付        赔付
-        (id,preComRes,finalPaymentRes,resPay)
+        (id,preCom,finalPayment,finalPaymentRes)
       })
       .toDF("id_slave","pre_com_new","final_payment_new","res_pay")
 
     val odsClaimDetail = odsClaimDetailOne.join(odsClaimDetailTwo,odsClaimDetailOne("id") ===odsClaimDetailTwo("id_slave"))
-      .selectExpr("case_no","policy_no","risk_date","report_date","risk_name","risk_cert_no","mobile","insured_company",
-        "pre_com_new","disable_level","scene","case_type","case_status","case_close_date","hos_benefits","medical_coverage",
-        "delay_payment","disable_death_payment","final_payment_new","res_pay")
+      .selectExpr("id_slave","case_no","policy_no","risk_date","report_date",
+        "risk_name","risk_cert_no","mobile","insured_company",
+        "cast(pre_com_new as decimal(14,4)) as pre_com_new",
+        "disable_level","scene","case_type","case_status",
+        "case_close_date","hos_benefits","medical_coverage",
+        "delay_payment","disable_death_payment",
+        "cast( final_payment_new as decimal(14,4)) as final_payment_new",
+        "cast( res_pay as decimal(14,4)) as res_pay")
 
     /**
       * 保单明细数据和理赔明细数据通过保单号关联
       */
-    val res = odsPolicyDetail.join(odsClaimDetail,odsPolicyDetail("policy_code") === odsClaimDetail("policy_no"),"leftouter")
-      .selectExpr("getUUID() as id","policy_id","policy_code","product_code","policy_status","case_no","policy_no as risk_policy_code",
-        "risk_date","report_date","risk_name","risk_cert_no","mobile","insured_company","cast(pre_com_new as decimal(14,4)) as pre_com","disable_level",
-        "scene","case_type","case_status","case_close_date","hos_benefits","medical_coverage","delay_payment","disable_death_payment",
-        "cast(final_payment_new as decimal(14,4)) as final_payment","cast(res_pay as decimal(14,4))","getNow() as dw_create_time")
+    val res = odsPolicyDetail.join(odsClaimDetail,odsPolicyDetail("policy_code") === odsClaimDetail("policy_no"))
+      .selectExpr("getUUID() as id","id_slave","policy_id","policy_code","product_code",
+        "policy_status","case_no","policy_no as risk_policy_code",
+        "risk_date","report_date","risk_name","holder_name","risk_cert_no",
+        "mobile","insured_company","cast(pre_com_new as decimal(14,4)) as pre_com",
+        "disable_level",
+        "scene","case_type","case_status","case_close_date","hos_benefits",
+        "medical_coverage","delay_payment","disable_death_payment",
+        "cast(final_payment_new as decimal(14,4)) as final_payment",
+        "cast(res_pay as decimal(14,4)) as res_pay","getNow() as dw_create_time")
 
-    res
 
+    //读取企业信息表
+    val odsEnterpriseDetail: DataFrame = sqlContext.sql("select ent_id,ent_name from " +
+      "odsdb.ods_enterprise_detail")
+
+    //读取客户归属销售表
+
+    val odsEntguzhuSalesmanDetail: DataFrame = sqlContext.sql("select ent_id as entid,ent_name as entname," +
+      "channel_id,channel_name from odsdb.ods_ent_guzhu_salesman_detail")
+
+    // 关联两个表
+    val enterAndsalesman: DataFrame = odsEnterpriseDetail.join(odsEntguzhuSalesmanDetail,
+      odsEnterpriseDetail("ent_id")
+        === odsEntguzhuSalesmanDetail("entid"),"leftouter")
+      .selectExpr("entid as ent_id","entname as ent_name","channel_id","channel_name")
+
+    // 将理赔表与保单明细表的结果 与 客户归属销售表和企业信息表的结果关联
+    val resEnd: DataFrame = res.join(enterAndsalesman, res("holder_name") === enterAndsalesman("ent_name"),
+      "leftouter").selectExpr(
+      "id","id_slave", "policy_id", "policy_code", "product_code", "policy_status",
+      "case_no", "risk_policy_code",
+      "risk_date", "report_date", "risk_name", "risk_cert_no", "mobile",
+      "ent_id","ent_name","insured_company","channel_id","channel_name",
+      "pre_com", "disable_level",
+      "scene", "case_type", "case_status", "case_close_date", "hos_benefits",
+      "medical_coverage",
+      "delay_payment", "disable_death_payment",
+      "final_payment",
+      "res_pay", "dw_create_time"
+    )
+    resEnd
   }
 
   /**
