1794b656fbfb11750592333037f8b556f8b245df
liuxiang <liuxiang@baozhunniu.com>
2019-10-12 18:16:53 +0800

    20191012 Spark整合ClickHouse

--- bzn_center_flat/job_ods_level/src/main/scala/bzn/other/sparkJdbcClickHouse.scala
+++ bzn_center_flat/job_ods_level/src/main/scala/bzn/other/sparkJdbcClickHouse.scala
@@ -1,14 +1,11 @@
 package bzn.other
 
-import java.sql.DriverManager
-import java.util.Properties
 import bzn.job.common.Until
 import bzn.util.SparkUtil
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.{DataFrame, SQLContext, SaveMode}
-import ru.yandex.clickhouse.ClickHouseDriver
-import ru.yandex.clickhouse.settings.ClickHouseProperties
+
 
 import scala.io.Source
 
@@ -26,50 +23,34 @@ object sparkJdbcClickHouse extends  SparkUtil with Until{
 
     val sc = sparkConf._2
     val hiveContext: HiveContext = sparkConf._4
-    val res: DataFrame = hiveContext.sql("select policy_id,policy_code from odsdb.ods_policy_detail")
+    val res: DataFrame = hiveContext.sql("select policy_id,policy_code,dw_create_time from odsdb.ods_policy_detail")
 
     writeTable(res,"odsdb.ods_policy_test")
     sc.stop()
 
+
   }
+
 
   /**
     *
     */
   def  writeTable(res:DataFrame,tableName: String): Unit ={
-    val properties: Properties = getProPerties()
-    //配置文件中的key 与 spark 中的 key 不同 所以 创建prop 按照spark 的格式 进行配置数据库
-
-   val clickDriver = new  ClickHouseDriver()
-    DriverManager.deregisterDriver(clickDriver)
-    val prop = new ClickHouseProperties()
-    prop.setHost("jdbc:clickhouse://172.16.11.100:8123/odsdb")
-    prop.setUser("default")
-    prop.setPassword("iaisYuX4")
     res.write
+        .mode(SaveMode.Append)
       .format("jdbc")
-      .option("","")
-      .option("isolationLevel","NONE") //设置事务
+      .option("url","jdbc:clickhouse://172.16.11.100:8123/odsdb")
+      .option("driver","cc.blynk.clickhouse.ClickHouseDriver")
       .option("numPartitions","1")//设置并发
-      .mode("Append")
+      .option("isolationLevel","NONE")
+      .option("dbtable",s"{$tableName}")
+      .option("user","default")
+      .option("password","iaisYuX4")
+      .option("batchsize","50000")
+      .save()
 
   }
 
-  /**
-    * 获取配置文件
-    * @return
-    */
-  def getProPerties() : Properties= {
-    val lines_source = Source.fromURL(getClass.getResource("/config_scala.properties")).getLines.toSeq
-    var properties: Properties = new Properties()
-    for (elem <- lines_source) {
-      val split = elem.split("==")
-      val key = split(0)
-      val value = split(1)
-      properties.setProperty(key,value)
-    }
-    properties
-  }
 
 
 
